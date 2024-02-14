61ed24f01ff670713f122d0d117780966b83cbd1
metta <anushametta071@gmail.com>
2024-02-13 22:50:28 -0500

    create account and login forgotpassword

---
+++ app/src/main/java/com/example/beautyhub/CreateAccount_Screen1.java
@@ -0,0 +1,14 @@
+package com.example.beautyhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccount_Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_screen1);
+    }
+}
